9ff3460a71e3a502d24e734c38829a48cb427c79
Felipe Rodriguez <mailbox@feliperodriguez.dev>
2024-03-17 18:02:00 +0100

    Add DemoAppPage component and server queries

---
+++ app/src/client/app/DemoAppPage.jsx
@@ -0,0 +1,8 @@
+export default function DemoAppPage(){
+    return (
+        <div>
+            <h1>Welcome to the Landing Page</h1>
+            <p>This is the landing page for the app.</p>
+        </div>
+    );
+};
