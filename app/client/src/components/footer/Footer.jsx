37089d59128eb24c0e838ac72cae662fd81cb8cc
Felipe Rodriguez <mailbox@feliperodriguez.dev>
2024-03-23 10:49:51 +0100

    Update footer content and styles

--- app/client/src/components/footer/Footer.jsx
+++ app/client/src/components/footer/Footer.jsx
@@ -1,20 +1,23 @@
 import React from "react";
+import { footerContent, footerTitle } from "./content";
 
 export const Footer = () => {
   return (
     <footer className="bg-gray-800 py-4">
       <div className="container mx-auto flex justify-between items-center py-3 my-4 border-t">
         <div className="w-full md:w-1/2 md:flex items-center">
-          <span className="text-muted text-white">© 2021 Company, Inc</span>
+          <span className="text-muted text-white">{footerTitle}</span>
         </div>
-        <div className="w-full md:w-1/2 md:flex items-center justify-end">
-          <a href="#" className="text-blue-500 hover:text-blue-800 mr-4">
-            Privacy Policy
+        
+        {footerContent.map((item) => (
+          <a
+            key={item.name}
+            href={item.href}
+            className="text-blue-500 hover:text-blue-800 mr-4"
+          >
+            {item.name}
           </a>
-          <a href="#" className="text-blue-500 hover:text-blue-800">
-            Terms of Service
-          </a>
-        </div>
+        ))}
       </div>
     </footer>
   );
