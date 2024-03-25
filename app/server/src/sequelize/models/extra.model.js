5c96a22096bd610f80f13b0a3c36082c4640765d
Felipe Rodriguez <mailbox@feliperodriguez.dev>
2024-03-25 16:19:30 +0100

    Add sequelize models for booking, extra, restaurant, and user

---
+++ app/server/src/sequelize/models/extra.model.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = sequelize => {
+  sequelize.define("extras", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.NOW,
+    },
+  });
+};
---
+++ app/server/src/sequelize/models/user.model.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = sequelize => {
+  sequelize.define("user", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      validate: {
+        isAlphanumeric: true,
+        max: 20,
+      },
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+      },
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: sequelize.NOW,
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    rol: {
+      type: DataTypes.STRING,
+      defaultValue: "user",
+    },
+  });
+};
