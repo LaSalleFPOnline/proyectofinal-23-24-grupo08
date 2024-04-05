f8d96464f0c17f3623db0d4f31ac569750be9bd3
Ignacio Sepúlveda <ignaciosepulveda129@gmail.com>
2024-04-05 22:49:08 +0200

    Creada la autenticación del login con JWT

--- app/server/src/express/app.js
+++ app/server/src/express/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const authRoutes = require("./routes/auth.js"); //borrar
 
 const routes = {
   // basics operations
@@ -9,7 +10,7 @@ const routes = {
   extra: require("./routes/crud"),
 
   // auth
-
+  userAuth: require("./routes/auth"),
   // add more routes here
 };
 
@@ -17,6 +18,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api", authRoutes); //yo
 
 function makeHandlerAwareOfAsyncErrors(handler) {
   return async function (req, res, next) {
--- app/server/src/express/routes/auth.js
+++ app/server/src/express/routes/auth.js
@@ -1,4 +1,4 @@
-const { User } = require("../../sequelize/models/user.model");
+const { models } = require("../../sequelize");
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
@@ -7,24 +7,35 @@ const jwt = require("jsonwebtoken");
 // Ruta para la solicitud de inicio de sesión
 router.post("/login", async (req, res) => {
   // Obtener datos de la solicitud
-  const username = req.body.username;
-  const password = req.body.password;
-  console.log("---------> username: ", username);
+  const { username, password } = req.body;
+
   try {
-    // Buscar usuario en la bd por nombre de usuario
-    console.log("---------_> User:", User);
-    const user = await User.findOne({
+    // Buscar usuario en la base de datos por nombre de usuario
+    const user = await models.user.findOne({
       where: {
         username,
       },
     });
-
-    if (user && user.password === password) {
-      res.status(200).json({ message: "Inicio de sesión exitoso" });
+    console.log("--------> password:", password);
+    console.log("-------->user password:", user.password);
+    if (user && password === user.password) {
+      // Si el usuario y la contraseña son correctos, generar el token JWT
+      jwt.sign({ user }, "secretKey", { expiresIn: "1h" }, (err, token) => {
+        if (err) {
+          res.status(500).json({ message: "Error al firmar el token" });
+        } else {
+          //res.json({ token }); comprobación de que funciona y tengo token.
+          res.status(200).json({
+            message: "Login correcto con token",
+            token,
+          });
+        }
+      });
     } else {
       res.status(401).json({ message: "Credenciales inválidas" });
     }
   } catch (error) {
+    // Manejar cualquier error interno
     console.error("Error al iniciar sesión", error);
     res.status(500).json({ message: "Error interno" });
   }
