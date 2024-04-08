93f0f240ffc1329916caf7cb636e7f19d5806867
Ignacio Sepúlveda <ignaciosepulveda129@gmail.com>
2024-04-08 20:48:30 +0200

    Modificacion del archivo con token JWT y con contraseña cifrada con bcrypt.

--- app/server/src/express/routes/auth.js
+++ app/server/src/express/routes/auth.js
@@ -2,6 +2,7 @@ const { models } = require("../../sequelize");
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 // Ruta para la solicitud de inicio de sesión
 router.post("/login", async (req, res) => {
@@ -16,26 +17,36 @@ router.post("/login", async (req, res) => {
       },
     });
 
-    if (user && password === user.password) {
-      // Si el usuario y la contraseña son correctos, generar el token JWT
-      jwt.sign({ user }, "secretKey", { expiresIn: "1h" }, (err, token) => {
-        if (err) {
-          res.status(500).json({ message: "Error al firmar el token" });
-        } else {
-          //res.json({ token }); comprobación de que funciona y tengo token.
-          res.status(200).json({
-            message: "Login correcto con token",
-            token,
-            status: "OK",
-          });
-        }
-      });
-    } else {
-      res.status(401).json({
-        message: "Credenciales inválidas",
-        status: "Error",
-      });
-    }
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) {
+        res.status(500).json({ message: "Error" });
+      } else {
+        bcrypt.compare(user.password, hash, (err, result) => {
+          if (err) {
+            res.status(500).json({ message: "Error" });
+          } else {
+            jwt.sign(
+              { user },
+              "secretKey",
+              { expiresIn: "1h" },
+              (err, token) => {
+                if (err) {
+                  res.status(500).json({ message: "Error al firmar el token" });
+                } else {
+                  res.status(200).json({
+                    message: "Login correcto con token",
+                    token,
+                    status: "OK",
+                  });
+                  //Comprobación de que imprime el hash correctamente
+                  //console.log("------>Hash: ", hash);
+                }
+              }
+            );
+          }
+        });
+      }
+    });
   } catch (error) {
     // Manejar cualquier error interno
     console.error("Error al iniciar sesión", error);
