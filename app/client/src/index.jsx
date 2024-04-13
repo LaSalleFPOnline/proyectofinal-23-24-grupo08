5f9481fd918d0486e28b53f141541b12b3c01b56
Felipe Rodriguez <mailbox@feliperodriguez.dev>
2024-04-13 10:19:01 +0200

    Fix import statements in index.jsx

--- app/client/src/index.jsx
+++ app/client/src/index.jsx
@@ -4,8 +4,6 @@ import 'the-new-css-reset/css/reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import { App } from './App';
-// import { LoginPage } from './pages/loginPage/LoginPage';
 import UserProvider from './Context/UserProvider';
 import AppRouter from './routes/AppRouter';
 
