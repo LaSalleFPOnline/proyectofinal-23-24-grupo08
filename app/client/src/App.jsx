da75b17cad7c9b2c5ebbdb6aedc1b654f48ce744
Felipe Rodriguez <mailbox@feliperodriguez.dev>
2024-04-13 16:17:04 +0200

    Remove unused App component

--- app/client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import { LandingPage } from './pages/landing/LandingPage';
-
-export const App = () => {
-    console.log('app .....');
-    return <>{/* <LandingPage /> */}</>;
-};
-
-export default App;
