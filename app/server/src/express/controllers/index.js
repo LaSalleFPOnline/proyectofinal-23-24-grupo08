45b3c392f34e4b3c65ff35daa87b4a91d17b09d5
dferran84 <dferran84@gmail.com>
2024-05-01 11:20:18 +0200

    Refactor endpoints and controllers

---
+++ app/server/src/express/controllers/index.js
@@ -0,0 +1,6 @@
+const bookingController = require('./bookingController');
+const restaurantController = require('./restaurantController');
+module.exports = {
+    bookingController,
+    restaurantController
+};
--- app/server/src/sequelize/models/booking.model.js
+++ app/server/src/sequelize/models/booking.model.js
@@ -1,70 +1,70 @@
-const { DataTypes } = require("sequelize");
+const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize, Sequelize) => {
-  const Booking = sequelize.define("booking", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    idRestaurant: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: "restaurants",
-        key: "id",
-      },
-    },
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    time: {
-      type: DataTypes.TIME,
-      allowNull: false,
-    },
-    guests: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    comments: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    phone: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    status: {
-      type: DataTypes.STRING,
-      defaultValue: "pending",
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      defaultValue: sequelize.NOW,
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      defaultValue: sequelize.NOW,
-    },
-  });
+    const Booking = sequelize.define('booking', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        idRestaurant: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'restaurants',
+                key: 'id'
+            }
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        time: {
+            type: DataTypes.TIME,
+            allowNull: false
+        },
+        guests: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        comments: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        status: {
+            type: DataTypes.STRING,
+            defaultValue: 'pending'
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: sequelize.NOW
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            defaultValue: sequelize.NOW
+        }
+    });
 
-  Booking.associate = (models) => {
-    Booking.hasOne(models.restaurant);
-    //Booking.hasOne(models.user);
-    Booking.hasMany(models.extra);
+    Booking.associate = (models) => {
+        Booking.hasOne(models.restaurant);
+        //Booking.hasOne(models.user);
+        Booking.hasMany(models.extra);
 
-    //Booking.belongsTo(models.user);
-    Booking.belongsTo(models.restaurant);
-  };
+        //Booking.belongsTo(models.user);
+        Booking.belongsTo(models.restaurant);
+    };
 
-  return Booking;
+    return Booking;
 };
